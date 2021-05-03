5e50b06e85ff10be0402ba4f2851a0e2a42db634
DevTheofanes <devtheofanes@gmail.com>
2021-05-03 12:38:15 -0300

    fix host bug

--- src/hooks/useUser.jsx
+++ src/hooks/useUser.jsx
@@ -6,7 +6,7 @@ import { history } from "../services/history";
 const UserContext = createContext([]);
 
 export function UserContextProvider({children}){
-  const host = "http://voucolar.bestapp.com.br:8002"
+  const host = "https://server.voucolar.com.br:8002"
   // const host = "http://127.0.0.1:3333"
 
   const [user, setUser] = useState({})
